1b94bf7fdb157426f2a79ee802ae04bcbe36ec42
FlocosDEV <FlocosDev@gmail.com>
2024-11-20 11:30:02 -0300

    Initial commit

---
+++ src/main/java/net/flocos/mushroomfy/MushroomfyClient.java
@@ -0,0 +1,10 @@
+package net.flocos.mushroomfy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MushroomfyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/flocos/mushroomfy/MushroomfyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.flocos.mushroomfy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MushroomfyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
